ce1aa81764ff31032a199260016e75903c94388c
Sumit Gupta <109888534+guptasumi@users.noreply.github.com>
2023-03-03 22:22:41 +0530

    Add files via upload

---
+++ snake game/moving _snake.py
@@ -0,0 +1,47 @@
+import pygame
+import time
+pygame.init()
+ 
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+ 
+dis = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Snake Game made by Sumit Gupta')
+ 
+game_over = False
+ 
+x1 = 200
+y1 = 200
+ 
+x1_change = 0       
+y1_change = 0
+ 
+clock = pygame.time.Clock()
+ 
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = 10
+                x1_change = 0
+ 
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(black)
+    pygame.draw.rect(dis,red, [x1, y1, 10, 10])
+    pygame.display.update()
+    clock.tick(30)
+pygame.quit()
+quit()
